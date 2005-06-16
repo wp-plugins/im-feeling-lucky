3a711b59062de5edb5b65e3a52fd4821b6101a20
sameerd <sameerd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-06-16 17:43:06 +0000

    Added GPL
    git-svn-id: https://plugins.svn.wordpress.org/im-feeling-lucky/trunk@2291 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lucky.php
+++ lucky.php
@@ -12,6 +12,22 @@ http://www.asymptomatic.net quicktag button template downloaded from
 http://redalt.com/downloads/
 */ 
 
+/*  Copyright 2004  Sameer D'Costa  (email : sameerd1977 at gmail dot com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 add_filter('admin_footer', 'google_tag');
 
