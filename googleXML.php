ab21c1540ac5352ae426fd5d85a138828bd751d5
sameerd <sameerd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-06-16 18:06:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/im-feeling-lucky/trunk@2293 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- googleXML.php
+++ googleXML.php
@@ -1,4 +1,20 @@
 <?php
+/*  Copyright 2004  Sameer D'Costa  (email : sameerd1977 at gmail dot com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 extract($_GET);
 $ss = "http://www.google.com/search?hl=en&q=" . 
 	urlencode($s) . "&btnI=I\'m+Feeling+Lucky";
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Staticize Reloaded ===
+Tags: quicktags, google
+Contributors: sameer
+
+The I'm feeling Lucky plugin puts in links to Google's top hit for text that you select while editing your post.
+
+== Installation ==
+
+See http://dcostanet.net/wordpress/2005/06/09/im-feeling-lucky/
+
+== Frequently Asked Questions ==
+
+= Why did you write this crap? =
+
+I was frequently performing this task again and again while writing posts, so I decided to write a plugin to make my life easier. 
+
+= You know it is complete crap right? =
+
+Yes, I'm sorry. 
+
